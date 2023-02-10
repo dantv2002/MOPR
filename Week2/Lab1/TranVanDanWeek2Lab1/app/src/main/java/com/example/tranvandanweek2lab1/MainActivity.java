cbdb558ebc1aa9cb5053448e46e293c1b0608b30
Văn Dân <danok123456@gmail.com>
2023-02-10 15:54:18 +0700

    week2 lab1

---
+++ Week2/Lab1/TranVanDanWeek2Lab1/app/src/main/java/com/example/tranvandanweek2lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tranvandanweek2lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoutlab1);
+    }
+}
