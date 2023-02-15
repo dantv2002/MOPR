04228fc99d00c4044ba2aaf6c796003cd60c1e96
Văn Dân <danok123456@gmail.com>
2023-02-16 02:07:34 +0700

    final lab2

--- Week2/Lab2/app/src/main/java/com/example/lab2week2/MainActivity.java
+++ Week2/Lab2/app/src/main/java/com/example/lab2week2/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addFunction(View view) {
+        // Do something in response to button click
+        int sum = 0;
+        EditText edtIpA = (EditText)findViewById(R.id.editTextTextInputA);
+        EditText edtIpB = (EditText)findViewById(R.id.editTextTextInputB);
+        TextView tvSum = (TextView)findViewById(R.id.textViewAaddB);
+        String ipA = edtIpA.getText().toString();
+        String ipB = edtIpB.getText().toString();
+        try{
+            sum = Integer.parseInt(ipA) + Integer.parseInt(ipB);
+            tvSum.setText(""+sum);
+        } catch (NumberFormatException e){
+            Log.e("Event Handling Error", "please fill all input");
+            return;
+        } catch (Exception ex){
+            return;
+        }
+        Log.i("Event Handling", "A + B = " + sum);
+    }
+
 }
