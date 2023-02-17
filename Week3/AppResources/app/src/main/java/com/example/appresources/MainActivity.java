e3491af8d8e6ae30621a1f9270091023acaf330e
Văn Dân <danok123456@gmail.com>
2023-02-17 14:54:20 +0700

    orientation change layout

--- Week3/AppResources/app/src/main/java/com/example/appresources/MainActivity.java
+++ Week3/AppResources/app/src/main/java/com/example/appresources/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appresources;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.i("landscape", "landscape");
+            setContentView(R.layout.activity_main_landscape);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Log.i("portrait", "portrait");
+            setContentView(R.layout.activity_main);
+        }
     }
 }
