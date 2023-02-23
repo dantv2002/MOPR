84a954db7d1c833c5c196253146a7c8d0220d8e7
Văn Dân <danok123456@gmail.com>
2023-02-24 00:10:50 +0700

    cfinishFragment Tutorials

--- Week3/FragmentTutorials/app/src/main/java/com/example/fragmenttutorials/MainActivity.java
+++ Week3/FragmentTutorials/app/src/main/java/com/example/fragmenttutorials/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.fragmenttutorials;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fristFragment = new FristFragment();
+        Fragment secondFragment = new SecondFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.flFragment, fristFragment, null)
+                .setReorderingAllowed(true)
+                .commit();
+
+        Button button1 = findViewById(R.id.btnFragment1);
+        Button button2 = findViewById(R.id.btnFragment2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.flFragment, fristFragment, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.flFragment, secondFragment, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 }
