bd4b52d585af8f65f4b383a0aad0be97df640d0e
Văn Dân <danok123456@gmail.com>
2023-02-24 14:02:18 +0700

    finish test Activity Life Cycle

--- Week4/ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ Week4/ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("onCreate", "From onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("onStart", "From onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("onResume", "From onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onStop", "From onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("onRestart", "From onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy", "From onDestroy");
     }
 }
