ae110d06aa9caf83338e993dd6ad9daccf49892a
Văn Dân <danok123456@gmail.com>
2023-03-03 02:45:02 +0700

    create third project - implicit intent

--- Week4/Implicit Intent/ImplicitIntentDemo/app/src/main/java/com/example/implicitintentdemo/MainActivity.java
+++ Week4/Implicit Intent/ImplicitIntentDemo/app/src/main/java/com/example/implicitintentdemo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.implicitintentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLaunchWebPagebuttonClicked(View v){
+        Log.i("Btn", "Clicked");
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fhqx.hcmute.edu.vn/"));
+        startActivity(intent);
     }
 }
