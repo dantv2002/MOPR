f8e01c423b6b0a2d04c4f55cde5af412b9d46f41
Văn Dân <danok123456@gmail.com>
2023-03-17 15:15:51 +0700

    finish practice 2

--- Week6/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/ExampleBroadcastReceiver.java
+++ Week6/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/ExampleBroadcastReceiver.java
@@ -11,14 +11,25 @@ public class ExampleBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
-            Toast.makeText(context, "Boot completed", Toast.LENGTH_LONG).show();
+//        PRACTICE 1
+//        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+//            Toast.makeText(context, "Boot completed", Toast.LENGTH_LONG).show();
+//        }
+//
+//        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+//            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_LONG).show();
+//        }
+//
+//        Log.e("ExampleBroadcastReceiver", "onReceive");
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            boolean noConnectivity = intent.getBooleanExtra(
+                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+            );
+            if (noConnectivity){
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Connected", Toast.LENGTH_LONG).show();
+            }
         }
-
-        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
-            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_LONG).show();
-        }
-
-        Log.e("ExampleBroadcastReceiver", "onReceive");
     }
 }
