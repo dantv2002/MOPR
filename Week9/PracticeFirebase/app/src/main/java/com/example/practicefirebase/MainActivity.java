64920f3db13acd0b2e6e22e33706b637e6b1a452
Văn Dân <danok123456@gmail.com>
2023-04-20 13:17:22 +0700

    finish practice 1

--- Week9/PracticeFirebase/app/src/main/java/com/example/practicefirebase/MainActivity.java
+++ Week9/PracticeFirebase/app/src/main/java/com/example/practicefirebase/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.practicefirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txtName, txtAge, txtPhone, txtHeight;
+    Button btnSave;
+    DatabaseReference reff;
+    Member member;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_datainsert);
+
+        txtName = findViewById(R.id.txtname);
+        txtAge = findViewById(R.id.txtage);
+        txtPhone = findViewById(R.id.txtphone);
+        txtHeight = findViewById(R.id.txtheight);
+        btnSave = findViewById(R.id.btnsave);
+        member = new Member();
+        reff = FirebaseDatabase.getInstance().getReference().child("Member");
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                member.setName(txtName.getText().toString().trim());
+                member.setAge(Integer.parseInt(txtAge.getText().toString().trim()));
+                member.setPhone(Long.parseLong(txtPhone.getText().toString().trim()));
+                member.setHeight(Float.parseFloat(txtHeight.getText().toString().trim()));
+
+                reff.push().setValue(member);
+                Toast.makeText(MainActivity.this,"insert data successfully",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
