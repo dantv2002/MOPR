bfb393379ba7d5940abdd4d31f1d0311f2f5c996
Văn Dân <danok123456@gmail.com>
2023-04-21 02:00:33 +0700

    finish project to practice firebase play music

--- Week9/Practice2/app/src/main/java/com/example/practice2/MainActivity.java
+++ Week9/Practice2/app/src/main/java/com/example/practice2/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.practice2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    }
+
+    public void playSong(View v){
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/practicefirebase-f0570.appspot.com/o/LaAnh-PhamLich.mp3?alt=media&token=0196d5de-1761-4ef2-8e36-f45a63447688");
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+
+            mediaPlayer.prepare();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
