2e96309f2a0ee629ed54d8ae07385a9068d22657
Văn Dân <danok123456@gmail.com>
2023-04-20 11:50:42 +0700

    create project to practice firebase

---
+++ Week9/PracticeFirebase/app/src/main/java/com/example/practicefirebase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.practicefirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
